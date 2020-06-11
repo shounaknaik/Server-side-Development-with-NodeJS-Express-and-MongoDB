a9eef6289dc195cf66443890221f80ef00c89d14
Shounak Naik <f20170835@goa.bits-pilani.ac.in>
2020-06-11 18:42:30 +0530

    Simple Node Module

--- index.js
+++ index.js
@@ -1,10 +1,7 @@
-var rect={
-    perimeter:(x,y)=>(2*(x+y)),
-    area:(x,y)=>(x*y)
-};
+var rect=require('./rectangle')
 
 function solveRect(l,b){
-    console.log("Solving for rectangle with l="l+"b= "+b);
+    console.log("Solving for rectangle with l="+l+"b= "+b);
 
     if(l<=0 || b<=0)
         console.log("Dimensions should be greater than 0");
---
+++ rectangle.js
@@ -0,0 +1,8 @@
+var rect={
+    perimeter:(x,y)=>(2*(x+y)),
+    area:(x,y)=>(x*y)
+};
+
+
+exports.perimeter =(x,y)=>(2*(x+y));
+exports.area =(x,y)=>(x*y);
